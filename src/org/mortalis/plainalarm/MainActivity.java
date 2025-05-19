f776d1114fb5033e2a6bbf72709475252b4f315b
mortalis13 <stangerroman@gmail.com>
2025-05-19 23:17:44 +0200

    exit on back press

--- src/org/mortalis/plainalarm/MainActivity.java
+++ src/org/mortalis/plainalarm/MainActivity.java
@@ -151,6 +151,11 @@ public class MainActivity extends AppCompatActivity {
     return super.onOptionsItemSelected(item);
   }
   
+  @Override
+  public void onBackPressed() {
+    finishAndRemoveTask();
+  }
+  
   
   // -----------------------------------------------------------
   
